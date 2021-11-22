25c75a00fc3ca239dd96de10a3cd257f106ee7cb
Microwave-WYB <david_wyb2001@outlook.com>
2021-11-21 23:47:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ document.body.addEventListener("mousemove", function (e) {
 });
 
 card.addEventListener("mouseenter", function (e) {
-  (cursor.style.transform = "scale(4)");
+  (cursor.style.transform = "scale(6)"),
+  (cursor.style.backgroundColor = "rgba(255, 255, 255, 0.0)");
 });
 
 card.addEventListener("mouseleave", function (e) {
   (cursor.style.transform = "scale(1)");
+  (cursor.style.backgroundColor = "white");
 });
 
 $(".floating-card-frame").mousemove(function (e) {
@@ -27,9 +29,13 @@ $(".floating-card-frame").mousemove(function (e) {
   let yShadowTranslate = (Math.round(yRelCenter * 10));
   card.style.transform = `translate(${xTranslatePercent-50}%, ${yTranslatePercent-50}%) scale(1.05)`;
   card.style.boxShadow = `${xShadowTranslate}px ${yShadowTranslate}px 20px rgba(0, 0, 0, 0.4), ${xShadowTranslate}px ${yShadowTranslate}px 16px rgba(43, 142, 255, 0.4)`;
+
+  let brightness = 1 + 0.3 * (1 - Math.sqrt(Math.pow(xRelCenter, 2) + Math.pow(yRelCenter, 2)));
+  card.style.filter = `brightness(${brightness})`;
 });
 
 $(".floating-card-frame").mouseleave(function () {
   card.style.transform = "translate(-50%, -50%)";
   card.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.5)";
+  card.style.filter = "brightness(1)";
 });
