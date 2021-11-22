dc805748c1e8baec6a014d6e0098ed49864a1660
Microwave-WYB <david_wyb2001@outlook.com>
2021-11-21 23:27:33 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let cursor = document.getElementById("cursor");
+let frame = document.querySelector(".floating-card-frame");
+let card = document.querySelector(".floating-card");
+
+document.body.addEventListener("mousemove", function (e) {
+  (cursor.style.left = e.clientX + "px"), (cursor.style.top = e.clientY + "px");
+});
+
+card.addEventListener("mouseenter", function (e) {
+  (cursor.style.transform = "scale(4)");
+});
+
+card.addEventListener("mouseleave", function (e) {
+  (cursor.style.transform = "scale(1)");
+});
+
+$(".floating-card-frame").mousemove(function (e) {
+  let offset = $(this).offset();
+  let x = e.pageX - offset.left;
+  let y = e.pageY - offset.top;
+  let xRelCenter = (x - $(this).width() / 2) / ($(this).width() / 2);
+  let yRelCenter = (y - $(this).height() / 2) / ($(this).height() / 2);
+  let maxTranslate = $(this).width() / 2 - $(".floating-card").width() / 2;
+  let xTranslatePercent = xRelCenter * maxTranslate / $(this).width() * 100;
+  let yTranslatePercent = yRelCenter * maxTranslate / $(this).height() * 100;
+  let xShadowTranslate = (Math.round(xRelCenter * 10));
+  let yShadowTranslate = (Math.round(yRelCenter * 10));
+  card.style.transform = `translate(${xTranslatePercent-50}%, ${yTranslatePercent-50}%) scale(1.05)`;
+  card.style.boxShadow = `${xShadowTranslate}px ${yShadowTranslate}px 20px rgba(0, 0, 0, 0.4), ${xShadowTranslate}px ${yShadowTranslate}px 16px rgba(43, 142, 255, 0.4)`;
+});
+
+$(".floating-card-frame").mouseleave(function () {
+  card.style.transform = "translate(-50%, -50%)";
+  card.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.5)";
+});
